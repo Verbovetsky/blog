717b061f2aba5de34e96574f87552597fe03f389
verbovetsky <ivandzob@gmail.com>
2016-12-08 19:40:56 +0200

    update

--- database.php
+++ database.php
@@ -3,6 +3,18 @@ define('MYSQL_SERVER', 'localhost');
 define('MYSQL_USER', 'root');
 define('MYSQL_PASSWORD', '');
 define('MYSQL_DB', 'blog');
+define('MYSQL_SERVER', 'localhost');
+define('MYSQL_USER', 'root');
+define('MYSQL_PASSWORD', '');
+define('MYSQL_DB', 'blog');
+define('MYSQL_SERVER', 'localhost');
+define('MYSQL_USER', 'root');
+define('MYSQL_PASSWORD', '');
+define('MYSQL_DB', 'blog');
+define('MYSQL_SERVER', 'localhost');
+define('MYSQL_USER', 'root');
+define('MYSQL_PASSWORD', '');
+define('MYSQL_DB', 'blog');
 
 function db_connect(){
     $link = mysqli_connect(MYSQL_SERVER, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB) 
