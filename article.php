ccc06db732ab01f87f927bb00bfea3269b2f8734
Verbovetsky <ivandzob@gmail.com>
2016-12-11 23:13:22 +0200

    Delete article.php
    5

--- article.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require_once("database.php");
-    require_once("models/articles.php");
-    
-    $link = db_connect();
-    $article = article_get($link, $_GET['id']);
-    include("views/article.php");
-?>
